df0671503e7b82b81ac6f253949a2b16b50e0734
siknyang <167048578+siknyang@users.noreply.github.com>
2024-06-18 11:30:28 +0900

    [Add] Scriptable Object 스크립트, 자동차 데이터 추가

---
+++ Assets/Scripts/ScriptableObject/Script/CarData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Obstacle", menuName = "Car")]
+public class CarData : ScriptableObject
+{
+    [Header("Info")]
+    public string carName;
+    public float speed;
+    public GameObject carPrefab;
+}
