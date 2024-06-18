6f4549753feaec8c1988558b5c11dfa6ed46df66
siknyang <167048578+siknyang@users.noreply.github.com>
2024-06-18 11:32:15 +0900

    [Update] CarsController.cs 필드 추가
    TODO: 코루틴을 사용한 동시다발적 랜덤 생성
    씬에서는 스크립트 부착 테스트 중

--- Assets/Scripts/Controller/CarsController.cs
+++ Assets/Scripts/Controller/CarsController.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CarsController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Object Pool Info")]
+    [SerializeField] int poolSize;
+    [SerializeField] ScriptableObject hatchback;
+    [SerializeField] ScriptableObject police;
+    [SerializeField] ScriptableObject sedan;
+    [SerializeField] ScriptableObject stationwagon;
+    [SerializeField] ScriptableObject taxi;
+
+    [Header("Spawn Position")]
+    [SerializeField] Transform left1;
+    [SerializeField] Transform right2;
+    [SerializeField] Transform left3;
+    [SerializeField] Transform right4;
+
+    private List<GameObject> activeCar = new List<GameObject>();
+
     void Start()
     {
-        
+        for (int i = 0; i < poolSize; i++)
+        {
+            SpawnCars();
+        }
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        // 생성 조건
+
+
+        // 제거 조건
+        //if ()
+    }
+
+    private void SpawnCars()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void RemoveCars()
+    {
+
     }
 }
